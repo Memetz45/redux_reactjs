ee99e18fbc35d8efabddfea981a60176caf71395
Memetz45 <slametnurhidayat45@gmail.com>
2023-03-02 22:12:30 +0700

    add file redux and install redux

---
+++ src/app/store.js
@@ -0,0 +1,12 @@
+import { applyMiddleware, combineReducers, compose, legacy_createStore as createStore } from "redux"
+import thunk from "redux-thunk";
+import counterReducer from "./features/Counter/reducer"
+
+let rootReducer = combineReducers ({
+    counter: counterReducer
+});
+
+const composeEnchancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+let store = createStore(rootReducer, composeEnchancers(applyMiddleware(thunk)));
+
+export default store;
